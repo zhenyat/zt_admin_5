178677cee0225871d79fc89b23b3ca3be94f1f5f
Zhenya Telyukov <telyukov@gmail.com>
2018-03-28 10:31:14 +0300

    Gemfile updated for 5.2

--- lib/zt_admin/init.rb
+++ lib/zt_admin/init.rb
@@ -4,12 +4,18 @@
 #   First phase to start Application Admin generation: gets generic Gemfile
 #
 #   28.01.2017  ZT
+#   28.03.2018  Last update
 ################################################################################
 
 module ZtAdmin
   begin
     FileUtils.cp "#{generic}/Gemfile", AppRoot
     puts colored GREEN, "\File 'Gemfile' has been updated"
+    
+    FileUtils.cp "#{generic}/gitignore", AppRoot
+    FileUtils.mv "#{AppRoot}/gitignore", "#{AppRoot}/.gitignore"
+    puts colored GREEN, "\File '.gitignore' has been updated"
+    
     puts colored(MAGENTA, "Run commands now:")
     puts colored(MAGENTA, "#{TAB2}bundle install")
     puts colored(MAGENTA, "#{TAB2}rails db:create")
